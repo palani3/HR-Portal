34b1b03bc2a314d80bf62ada5a27a17e448ad6e3
Palani K (TERAWE CORPORATION) <v-palanik@microsoft.com>
2024-04-19 11:30:21 +0530

    login page

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR hello palani
+          Edit <code>src/App.tsx</code> and save to test HMR hello palanisad
         </p>
       </div>
       <p className="read-the-docs">
