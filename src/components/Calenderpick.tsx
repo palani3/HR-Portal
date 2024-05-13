5d38fbdba82bcfaf055f0d212a68db9841c2d3b9
Palani K (TERAWE CORPORATION) <v-palanik@microsoft.com>
2024-05-13 10:40:10 +0530

    dashboard

--- src/components/Calenderpick.tsx
+++ src/components/Calenderpick.tsx
@@ -1,9 +1,24 @@
 import React, { useState } from "react";
 import "react-modern-calendar-datepicker/lib/DatePicker.css";
 import { Calendar } from "react-modern-calendar-datepicker";
+import { GoDotFill } from "react-icons/go";
 import "./calender.css";
+import { render } from "react-dom";
+
+function footer(){
+  return(
+    <div className="flex  px-10 py-2">
+      <GoDotFill color="#EF2B1F" size={18} />
+      <p className="text-sm mr-10">Govt Holiday</p>
+     
+      <GoDotFill color="#1E88E5" size={18}  />
+      <p className="text-sm">leave</p>
+  
 
 
+    </div>
+  )
+}
 export default function Calenderpick() {
   const [selectedDayRange, setSelectedDayRange] = useState({from:null,to:null});
   const handleDateChange = ({ from, to }) => {
@@ -21,9 +36,15 @@ export default function Calenderpick() {
         calendarTodayClassName="responsive-calendar"
         // colorPrimaryLight="rgba(75, 207, 250, 0.4)" // and this
         shouldHighlightWeekends
+        // slideAnimationDuration="1s"
+        renderFooter={footer}
       />
-        <p>From: {selectedDayRange.from ? `-${selectedDayRange.from.day}-${selectedDayRange.from.month}-${selectedDayRange.from.year}` : 'Not selected'}</p>
+       
+
+      <div className="flex p-2">
+        <p className="mx-2">From: {selectedDayRange.from ? `-${selectedDayRange.from.day}-${selectedDayRange.from.month}-${selectedDayRange.from.year}` : 'Not selected'}</p>
         <p>To: {selectedDayRange.to ? `${selectedDayRange.to.day}-${selectedDayRange.to.month}-${selectedDayRange.from.year}` : 'Not selected'}</p>
+    </div>
     </div>
   );
 }
--- src/pages/other/DateTime.tsx
+++ src/pages/other/DateTime.tsx
@@ -1,11 +1,37 @@
 import React from "react";
+import moment from 'moment';
 
 export default function DateTime({ name }) {
+  const currentDate = moment().format('DD-MMM-YYYY');
   return (
-    <div className="grid grid-cols-3  h-28 text-center content-center bg-slate-500 rounded-lg mx-5 p-5 ">
-      <div className="">Date and time</div>
-      <div>01 {name}</div>
-      <div>03</div>
+    <div className="grid grid-cols-3  bg-gradient-to-br from-green-200 via-blue-400 to-blue-800 h-28 text-center content-center rounded-lg mx-5 p-5 ">
+      <div className="text-white">Date and time</div>
+      <div className="grid justify-start text-white"><p className="text-white"> {currentDate}</p>
+      <p className="text-xl  font-bold ">Today</p></div>
+
+      <div className="grid  text-sm"> 
+             <div className="flex justify-end text-white">
+              <div className="">
+        <p className=" mx-4 ">Present-on time</p>
+       <p className="text-lg mt-3">80</p>
+        </div>
+        <div className="">
+        <p className=" mx-4 ">Late</p>
+       <p className="text-lg mt-3 ">20</p>
+        </div>
+        <div className="">
+        <p className=" mx-4 ">Absent</p>
+       <p className="text-lg mt-3">7</p>
+        </div>
+        <div className="">
+        <p className=" mx-4 ">Leave</p>
+       <p className="text-lg mt-3">3</p>
+        </div>
+        </div>
+       
+        
+        
+      </div>
       {/* <div>Date and time</div> */}
     </div>
   );
